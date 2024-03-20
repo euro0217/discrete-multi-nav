d1a6a189fceeff5bf8c659730f7f9225f6b591ca
YuhiroSato <scite.cogitate@gmail.com>
2024-03-20 09:54:21 +0900

    dijkstra for next reservation

--- src/pathfind/common.rs
+++ src/pathfind/common.rs
@@ -1,4 +1,4 @@
-use std::{collections::{HashMap, HashSet}, hash::Hash, ops::{Add, Index}, slice::Iter, vec::IntoIter};
+use std::{collections::{HashMap, HashSet}, fmt::Debug, hash::{Hash, Hasher}, ops::{Add, Index}, slice::Iter, vec::IntoIter};
 
 use num_traits::Zero;
 
@@ -21,20 +21,13 @@ impl<N: Eq + Hash + Clone> MultipleEnds<N> {
     pub fn end_index(&self, node: &N) -> Option<usize> { self.ends.get(node).copied() }
 }
 
+#[derive(Debug)]
 pub struct Path<N: Eq + Clone + Hash, C: Zero + Ord + Copy + Hash> {
     nodes: Vec<(N, C)>,
 }
 
 impl<N: Eq + Clone + Hash, C: Zero + Ord + Copy + Hash> Path<N, C> {
-    pub(crate) fn new(nodes: Vec<NodeCost<Node<N>, C>>) -> Self {
-        let nodes = nodes.into_iter()
-            .filter_map(|node| {
-                match node.node() {
-                    Node::Node(n) => Some((n.clone(), node.cost())),
-                    Node::Dest => None,
-                }
-            })
-            .collect::<Vec<_>>();
+    pub(crate) fn new(nodes: Vec<(N, C)>) -> Self {
         Self { nodes }
     }
 
@@ -71,7 +64,7 @@ impl<N: Eq + Clone + Hash, C: Zero + Ord + Copy + Hash> PartialEq for NodeCost<N
 }
 
 impl<N: Eq + Clone + Hash, C: Zero + Ord + Copy + Hash> Hash for NodeCost<N, C> {
-    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
+    fn hash<H: Hasher>(&self, state: &mut H) {
         self.node.hash(state);
     }
 }
@@ -98,3 +91,79 @@ impl<C: Zero + Ord + Copy> Add<Self> for Cost<C> {
         Self { i: self.i + rhs.i, c: self.c + rhs.c }
     }
 }
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub enum RCost<C: Zero + Ord + Copy + Hash> {
+    Cost { cost: C, r: C, blocked: bool },
+    Add { dc: C, max: C },
+    AddBlocked { dc: C },
+}
+
+impl<C: Zero + Ord + Copy + Hash> Zero for RCost<C> {
+    fn zero() -> Self { Self::Cost { cost: C::zero(), r: C::zero(), blocked: false }}
+    fn is_zero(&self) -> bool {
+        if let Self::Cost { cost, r, blocked } = self {
+            cost.is_zero() && r.is_zero() && !blocked
+        } else {
+            false
+        }
+    }
+}
+
+impl<C: Zero + Ord + Copy + Hash> Add for RCost<C> {
+    type Output = Self;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        match self {
+            RCost::Cost { cost, r, blocked } => {
+                match rhs {
+                    RCost::Add { dc, max } => {
+                        if blocked || r + dc > max {
+                            RCost::Cost { cost: cost + dc, r, blocked: true }
+                        } else {
+                            RCost::Cost { cost, r: r + dc, blocked: false }
+                        }
+                    },
+                    RCost::AddBlocked { dc } => {
+                        RCost::Cost { cost: cost + dc, r, blocked: true }
+                    },
+                    RCost::Cost { cost: cost2, r: r2, blocked: blocked2 } => {
+                        RCost::Cost { cost: cost + cost2, r: r + r2, blocked: blocked || blocked2 }
+                    },
+                }
+            },
+            _ => panic!()
+        }
+    }
+}
+
+// pub struct Test<const N: usize> {
+//     a: [i32; N]
+// }
+
+
+// #[derive(Debug, Clone)]
+// pub(crate) struct RNode<N: Eq + Clone + Hash> {
+//     node: N,
+//     cost: bool,
+// }
+
+// impl<N: Eq + Clone + Hash> RNode<N> {
+//     pub fn new(node: N, cost: bool) -> Self {
+//         Self { node, cost }
+//     }
+//     pub fn node(&self) -> &N { &self.node }
+//     pub fn cost(&self) -> bool { self.cost }
+// }
+
+// impl<N: Eq + Clone + Hash> Eq for RNode<N> {}
+
+// impl<N: Eq + Clone + Hash> PartialEq for RNode<N> {
+//     fn eq(&self, other: &Self) -> bool { self.node == other.node }
+// }
+
+// impl<N: Eq + Clone + Hash> Hash for RNode<N> {
+//     fn hash<H: Hasher>(&self, state: &mut H) {
+//         self.node.hash(state);
+//     }
+// }
--- src/pathfind/dijkstra.rs
+++ src/pathfind/dijkstra.rs
@@ -3,7 +3,54 @@ use std::hash::Hash;
 use num_traits::Zero;
 use pathfinding::directed::dijkstra::dijkstra;
 
+use crate::pathfind::common::RCost;
+
 use super::common::{Cost, MultipleEnds, Node, NodeCost, Path};
+
+pub fn dijkstra_for_next_reservation<N, C, S, FN, IN, IS, FS>(start: N, ends: &MultipleEnds<N>, mut successors: FN, seats_reservation: FS, max_reservation_cost: C)
+-> Option<Path<N, C>> where
+    N: Eq + Hash + Clone,
+    C: Zero + Ord + Copy + Hash,
+    S: Eq + Clone,
+    FN: FnMut(&N) -> IN,
+    IN: IntoIterator<Item = (N, C, IS)>,
+    IS: Iterator<Item = S>,
+    FS: Fn(&S) -> bool,
+{
+    if ends.is_empty() { return None }
+
+    let successors = |n: &N| {
+        successors(n)
+            .into_iter()
+            .map(|(m, dc, ss)| {
+                if ss.into_iter().all(|s| seats_reservation(&s)) {
+                    (m, RCost::Add { dc, max: max_reservation_cost })
+                } else {
+                    (m, RCost::AddBlocked { dc })
+                }
+            })
+    };
+
+    dijkstra_for_multiple_ends(&start, ends, successors)
+        .and_then(|path| Some(Path::new(
+            path.into_iter()
+                .take_while(|(_, c)| {
+                    if let RCost::Cost { cost: _, r: _, blocked } = c {
+                        !blocked
+                    } else {
+                        false
+                    }
+                })
+                .map(|(n, c)| {
+                    if let RCost::Cost { cost, r, blocked: _ } = c {
+                        (n, cost + r)
+                    } else {
+                        panic!()
+                    }
+                })
+                .collect::<Vec<_>>()
+        )))
+}
 
 pub fn dijkstra_for_multiple_ends<N, C, FN, IN>(start: &N, ends: &MultipleEnds<N>, mut successors: FN)
 -> Option<Path<N, C>> where
@@ -30,19 +77,28 @@ pub fn dijkstra_for_multiple_ends<N, C, FN, IN>(start: &N, ends: &MultipleEnds<N
     };
 
     dijkstra(&NodeCost::new(Node::Node(start.clone()), C::zero()), successors, |n| n.node() == &Node::Dest)
-        .and_then(|(path, _)| Some(Path::new(path)))
+        .and_then(|(path, _)| 
+            Some(Path::new(path.into_iter()
+                .filter_map(|node| {
+                    match node.node() {
+                        Node::Node(n) => Some((n.clone(), node.cost())),
+                        Node::Dest => None,
+                    }
+                })
+                .collect::<Vec<_>>()
+        )))
 }
 
 #[cfg(test)]
 mod tests {
     use std::collections::HashSet;
 
-    use crate::pathfind::common::MultipleEnds;
+    use crate::pathfind::{common::MultipleEnds, dijkstra::dijkstra_for_next_reservation};
 
     use super::dijkstra_for_multiple_ends;
 
     #[test]
-    fn test0() {
+    fn multiple_ends_test0() {
         let ends = MultipleEnds::new(&vec![]);
         let successors = |&(x, y): &_| vec![(x + 1, y), (x, y + 1), (x - 1, y), (x, y - 1)].into_iter().map(|p| (p, 1));
         
@@ -50,7 +106,7 @@ mod tests {
     }
 
     #[test]
-    fn test1() {
+    fn multiple_ends_test1() {
         let ends = MultipleEnds::new(&vec![HashSet::from([(5, 1)])]);
         let successors = |&(x, y): &_| vec![(x + 1, y), (x, y + 1), (x - 1, y), (x, y - 1)].into_iter().map(|p| (p, 1));
         
@@ -64,7 +120,7 @@ mod tests {
     }
 
     #[test]
-    fn test2() {
+    fn multiple_ends_test2() {
         let ends = MultipleEnds::new(&vec![HashSet::from([(6, -1)]), HashSet::from([(5, 1)])]);
         let successors = |&(x, y): &_| vec![(x + 1, y), (x, y + 1), (x - 1, y), (x, y - 1)].into_iter().map(|p| (p, 1));
         
@@ -77,7 +133,7 @@ mod tests {
     }
 
     #[test]
-    fn test3() {
+    fn multiple_ends_test3() {
         let ends = MultipleEnds::new(&vec![HashSet::from([(6, -1), (-1, 7)]), HashSet::from([(5, 1)])]);
         let successors = |&(x, y): &_| vec![(x + 1, y), (x, y + 1), (x - 1, y), (x, y - 1)].into_iter().map(|p| (p, 1));
         
@@ -90,7 +146,7 @@ mod tests {
     }
 
     #[test]
-    fn test4() {
+    fn multiple_ends_test4() {
         let ends = MultipleEnds::new(&vec![HashSet::from([(13, 10)])]);
         let successors = |&(x, y): &_| vec![(x + 1, y), (x, y + 1), (x - 1, y), (x, y - 1)]
             .into_iter()
@@ -100,7 +156,7 @@ mod tests {
     }
 
     #[test]
-    fn test5() {
+    fn multiple_ends_test5() {
         let ends = MultipleEnds::new(&vec![HashSet::from([(13, 10)]), HashSet::from([(6, -1)])]);
         let successors = |&(x, y): &_| vec![(x + 1, y), (x, y + 1), (x - 1, y), (x, y - 1)]
             .into_iter()
@@ -112,5 +168,69 @@ mod tests {
         assert_eq!(path.len(), 9);
         assert_eq!(path[0], ((2, 3), 0));
         assert_eq!(path[path.len() - 1], ((6, -1), 8));
+    }
+
+    #[test]
+    fn next_seats_test1() {
+        let s3x3 = |x: i32, y: i32| [(x, y), (x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1), (x + 1, y + 1), (x - 1, y + 1), (x + 1, y + 1), (x - 1, y - 1)].into_iter();
+
+        let successors = |&(x, y): &_| vec![(x + 1, y), (x, y + 1), (x - 1, y), (x, y - 1)]
+            .into_iter()
+            .filter_map(|(x, y): (i32, i32)| if 0 <= x && x < 10 && 0 <= y && y < 10 {
+                Some(((x, y), 1, s3x3(x, y))) } else { None }
+            );
+
+        let seats_reservation = |&(x, y): &_| {
+            match x {
+                ..=2 => y < 4,
+                ..=5 => y < 4 || y >= 6,
+                _ => y >= 6,
+            }
+        };
+
+        let cases = [
+            ((1, 2), (4, 6), 2, Some(vec![(1, 2), (2, 2), (3, 2)])),
+            ((1, 2), (4, 6), 3, Some(vec![(1, 2), (2, 2), (3, 2), (4, 2)])),
+            ((1, 2), (4, 6), 4, Some(vec![(1, 2), (2, 2), (3, 2), (4, 2)])),
+            ((4, 2), (4, 6), 3, Some(vec![(4, 2)])),
+            ((4, 1), (5, 7), 3, Some(vec![(4, 1), (4, 2)])),
+            ((4, 1), (5, 7), 0, Some(vec![(4, 1)])),
+            ((8, 7), (1, 5), 2, Some(vec![(8, 7), (7, 7), (6, 7)])),
+            ((8, 7), (11, 6), 2, None),
+        ];
+
+        //
+        // 9 x x x
+        // 8 x x x
+        // 7 x x x   
+        // 6 x x x   e
+        // 5 x x x x x x x x x x
+        // 4 x x x x x x x x x x
+        // 3 + + +       x x x x
+        // 2 + s - - -   x x x x
+        // 1 + + +       x x x x
+        // 0             x x x x
+        //   0 1 2 3 4 5 6 7 8 9
+        //
+        // s: start, e: end (for first 3 cases)
+        // x: cannot be entered
+
+
+        for (start, ends, len, expected) in cases {
+            let ends = MultipleEnds::new(&vec![HashSet::from([ends])]);
+            let actual = dijkstra_for_next_reservation(start, &ends, successors, seats_reservation, len);
+
+            match (expected, actual) {
+                (None, None) => {},
+                (None, Some(_)) => panic!("expected None, but Some"),
+                (Some(_), None) => panic!("expected Some, but None"),
+                (Some(e), Some(a)) => {
+                    assert_eq!(e.len(), a.len());
+                    for i in 0..e.len() {
+                        assert_eq!((e[i], i), a[i]);
+                    }
+                },
+            }
+        }
     }
 }
